f5ec60ac693a3b0548837666f9751c40dbafbc12
George Satellite <lunaerxs@gmail.com>
2024-01-30 22:10:41 +0200

    render triangle

--- src/Renderer.h
+++ src/Renderer.h
@@ -6,8 +6,13 @@ class Renderer
         Renderer( MTL::Device* pDevice );
         ~Renderer();
         void draw( MTK::View* pView );
-
+    
     private:
+        void _buildShaders();
+        void _buildBuffers();
         MTL::Device* _pDevice;
         MTL::CommandQueue* _pCommandQueue;
+        MTL::RenderPipelineState* _pPSO;
+        MTL::Buffer* _pVertexPositionsBuffer;
+        MTL::Buffer* _pVertexColorsBuffer;
 };
